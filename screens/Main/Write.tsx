a42800dd37994eda3fffbe6969a8eb64fb68acf9
Andrew Shen <azshen23@gmail.com>
2022-09-06 17:21:38 -0400

    init commit

---
+++ screens/Main/Write.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Write = () => {
+  return (
+    <View>
+      <Text>Write</Text>
+    </View>
+  );
+};
+
+export default Write;
